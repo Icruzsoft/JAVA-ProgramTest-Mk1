ef5f485243c71edb868e073d4afd4e7733d4ddc9
Isaac Cruz Cuevas <Icruzsoft@hotmail.com>
2024-04-04 08:19:18 -0600

    Mk1 ClassFlix

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido(a) a Screen March");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDelanzamiento = 1999;
+        boolean incluidoEnELPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.8 + 9.0) / 3;
+        System.out.println(media);
+
+    }
+
+}
